0de3385e9ad70a2c5b8034d643fda0f51c04a1b5
Tami Williams <tami@asktami.com>
2019-11-25 11:05:50 -0500

    removed test notes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -121,18 +121,6 @@ class SearchBar extends Component {
 						</Link>
 					</div>
 				</form>
-				<br />
-				<div className="developer-note">
-					Note: during this test:
-					<br />
-					Clicking on the "Sessions" link loads default data overriding any
-					records added/removed from schedule by clicking on the stars.
-					<br />
-					Stars also won't work on the Session Detail page.
-					<br />
-					Reloading the browser window also resets everything to the default
-					data.
-				</div>
 			</>
 		);
 	}
