c211aac847928089f46fd9331a4f1546f20c1898
Tami Williams <tami@asktami.com>
2019-11-26 15:06:13 -0500

    can not see single sessionand its comments

--- src/services/session-api-service.js
+++ src/services/session-api-service.js
@@ -6,14 +6,32 @@ const SessionApiService = {
 		// unprotected endpoint
 		// will work even when not logged in
 		// pass AuthToken so can use loginUserId when logged in to join sessions + schedule records
-		return fetch(`${config.API_ENDPOINT}/sessions/`, {
-			headers: {
-				'content-type': 'application/json',
-				authorization: `bearer ${TokenService.getAuthToken()}`
-			}
-		}).then(res =>
-			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-		);
+
+		console.log('session-api-service Check AUTH TOKEN');
+
+		if (TokenService.hasAuthToken()) {
+			console.log('session-api-service HAS AUTH TOKEN');
+
+			return fetch(`${config.API_ENDPOINT}/sessions/`, {
+				headers: {
+					'content-type': 'application/json',
+					authorization: `bearer ${TokenService.getAuthToken()}`
+				}
+			}).then(res =>
+				!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+			);
+		} else {
+			console.log('session-api-service NO AUTH TOKEN');
+
+			return fetch(`${config.API_ENDPOINT}/sessions/`, {
+				headers: {
+					'content-type': 'application/json',
+					authorization: `none`
+				}
+			}).then(res =>
+				!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+			);
+		}
 	},
 	getSchedule() {
 		// protected endpoint
@@ -26,20 +44,20 @@ const SessionApiService = {
 		// this will ALWAYS send the loginUserId BECAUSE of the authoriztion header, which has the AuthToken, which is set at time of 1st login with the user record id
 		// see auth-api-service
 
-		console.log('CLIENT getting schedule');
-		if (TokenService.hasAuthToken()) {
-			return fetch(`${config.API_ENDPOINT}/schedule`, {
-				headers: {
-					'content-type': 'application/json',
-					authorization: `bearer ${TokenService.getAuthToken()}`
-				}
-			}).then(res =>
-				!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-			);
-		}
+		return fetch(`${config.API_ENDPOINT}/schedule`, {
+			headers: {
+				'content-type': 'application/json',
+				authorization: `bearer ${TokenService.getAuthToken()}`
+			}
+		}).then(res =>
+			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+		);
 	},
 	getSession(session_id) {
 		// protected endpoint
+
+		console.log('session-api-service session_id = ', session_id);
+
 		return fetch(`${config.API_ENDPOINT}/sessions/${session_id}`, {
 			headers: {
 				'content-type': 'application/json',
