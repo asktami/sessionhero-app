a952ec60873fc5cc06b358120fe3f0b277edf791
Tami Williams <tami@asktami.com>
2019-11-26 02:59:16 -0500

    bug - need to pass loginUserId to sessions

--- src/services/session-api-service.js
+++ src/services/session-api-service.js
@@ -3,29 +3,11 @@ import TokenService from './token-service';
 
 const SessionApiService = {
 	getSessions() {
-		// unprotected endpoint
-		return fetch(`${config.API_ENDPOINT}/sessions`, {
-			headers: {
-				'content-type': 'application/json'
-			}
-		}).then(res =>
-			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-		);
-	},
-	getSchedule() {
-		// protected endpoint
-
-		// only get schedule if have loginUserId
 		if (TokenService.hasAuthToken()) {
-			// loginUserId is retrieved by API when it processes the AuthToken, via jwt-auth.js, which finds the user record using the username stored in the AuthToken and then adds the user record to the req object (i.e req.user = user)
-			// so can get loginUserId via req.user.id
-
-			// this will ALWAYS send the loginUserId BECAUSE of the authoriztion header, which has the AuthToken, which is set at time of 1st login with the user record id
-			// see auth-api-service
-
-			console.log('CLIENT getting schedule');
-
-			return fetch(`${config.API_ENDPOINT}/schedule`, {
+			// unprotected endpoint
+			// will work even when not logged in
+			// pass AuthToken so can use loginUserId when logged in to join sessions + schedule records
+			return fetch(`${config.API_ENDPOINT}/sessions`, {
 				headers: {
 					'content-type': 'application/json',
 					authorization: `bearer ${TokenService.getAuthToken()}`
@@ -33,7 +15,38 @@ const SessionApiService = {
 			}).then(res =>
 				!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
 			);
+		} else {
+			return fetch(`${config.API_ENDPOINT}/sessions`, {
+				headers: {
+					'content-type': 'application/json'
+				}
+			}).then(res =>
+				!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+			);
 		}
+	},
+	getSchedule() {
+		// protected endpoint
+
+		// only get schedule if have loginUserId
+		if (TokenService.hasAuthToken()) {
+		}
+		// loginUserId is retrieved by API when it processes the AuthToken, via jwt-auth.js, which finds the user record using the username stored in the AuthToken and then adds the user record to the req object (i.e req.user = user)
+		// so can get loginUserId via req.user.id
+
+		// this will ALWAYS send the loginUserId BECAUSE of the authoriztion header, which has the AuthToken, which is set at time of 1st login with the user record id
+		// see auth-api-service
+
+		console.log('CLIENT getting schedule');
+
+		return fetch(`${config.API_ENDPOINT}/schedule`, {
+			headers: {
+				'content-type': 'application/json',
+				authorization: `bearer ${TokenService.getAuthToken()}`
+			}
+		}).then(res =>
+			!res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+		);
 	},
 	getSession(session_id) {
 		// protected endpoint
