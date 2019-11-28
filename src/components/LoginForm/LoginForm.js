bbbb2b39f0595bce21e027d52990fd6b03b45e35
Tami Williams <tami@asktami.com>
2019-11-27 20:00:29 -0500

    set loginUserId and get schedule

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,7 +3,6 @@ import AppContext from '../../contexts/AppContext';
 
 import TokenService from '../../services/token-service';
 import AuthApiService from '../../services/auth-api-service';
-import SessionApiService from '../../services/session-api-service';
 
 export default class LoginForm extends Component {
 	static contextType = AppContext;
@@ -14,24 +13,25 @@ export default class LoginForm extends Component {
 
 	state = { error: null };
 
-	handleSubmitBasicAuth = ev => {
-		ev.preventDefault();
-		const { username, password } = ev.target;
+	// UNUSED
+	// handleSubmitBasicAuth = e => {
+	// 	e.preventDefault();
+	// 	const { username, password } = e.target;
 
-		// create the login auth token and save it in localStorage
-		TokenService.saveAuthToken(
-			TokenService.makeBasicAuthToken(username.value, password.value)
-		);
+	// 	// create the login auth token and save it in localStorage
+	// 	TokenService.saveAuthToken(
+	// 		TokenService.makeBasicAuthToken(username.value, password.value)
+	// 	);
 
-		username.value = '';
-		password.value = '';
-		this.props.onLoginSuccess();
-	};
+	// 	username.value = '';
+	// 	password.value = '';
+	// 	this.props.onLoginSuccess();
+	// };
 
-	handleSubmitJwtAuth = ev => {
-		ev.preventDefault();
+	handleSubmitJwtAuth = e => {
+		e.preventDefault();
 		this.setState({ error: null });
-		const { username, password } = ev.target;
+		const { username, password } = e.target;
 
 		AuthApiService.postLogin({
 			username: username.value,
@@ -42,19 +42,24 @@ export default class LoginForm extends Component {
 				password.value = '';
 
 				console.log('loginForm loginUserId = ', JSON.stringify(res.user_id));
+
 				// save loginUserId
 				this.context.setLoginUserId(res.user_id);
+				// QUESTION - should this be part of the token function?
 
 				TokenService.saveAuthToken(res.authToken);
+
 				this.props.onLoginSuccess();
 			})
-			.then(() => {
-				// get schedule immediately upon login
-				// TBD - schedule is NOT being stored in context????  If reload page its gone!!!
-				SessionApiService.getSchedule()
-					.then(this.context.setScheduleList)
-					.catch(this.context.setError);
-			})
+
+			// QUESTION - should I set scheduleList on login?
+			// TBD
+			// .then(() => {
+			// 	// get schedule immediately upon login
+			// 	SessionApiService.getSchedule()
+			// 		.then(this.context.setScheduleList)
+			// 		.catch(this.context.setError);
+			// })
 
 			.catch(res => {
 				this.setState({ error: res.error });
