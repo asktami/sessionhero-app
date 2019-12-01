c61d090e34e9343123bb00392647baaafa0bfab9
Tami Williams <tami@asktami.com>
2019-11-30 20:40:41 -0500

    link to heroku database

--- src/config.js
+++ src/config.js
@@ -1,21 +1,15 @@
-// export default {
-// 	API_ENDPOINT: 'http://localhost:8000/api',
-// 	TOKEN_KEY: 'sessionhero-client-auth-token'
-// };
-
-// hosted using json db
-// https://cors-anywhere.herokuapp.com/
-// export default {
-// 	DATASOURCE: `json`,
-// 	API_ENDPOINT: `https://my-json-server.typicode.com/asktami/sessionhero-app`,
-// 	AUTH_ENDPOINT: `https://reqres.in/api/login`,
-// 	TOKEN_KEY: 'sessionhero-client-auth-token'
-// };
-
-// hosted locally using PostgreSQL db
+// hosted on Heroku using PostgreSQL db
 export default {
 	DATASOURCE: `postgresql`,
-	API_ENDPOINT: `http://localhost:8000/api`,
-	AUTH_ENDPOINT: `http://localhost:8000/api`,
+	API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api/`,
+	AUTH_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api/`,
 	TOKEN_KEY: 'sessionhero-client-auth-token'
 };
+
+// hosted locally using PostgreSQL db
+// export default {
+// 	DATASOURCE: `postgresql`,
+// 	API_ENDPOINT: `http://localhost:8000/api`,
+// 	AUTH_ENDPOINT: `http://localhost:8000/api`,
+// 	TOKEN_KEY: 'sessionhero-client-auth-token'
+// };
