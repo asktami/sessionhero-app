786c1b98219ea3ea62424bc4e39f715a02a61b42
Tami Williams <tami@asktami.com>
2019-12-02 10:14:18 -0500

    added password instructions

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
 import { Required } from '../Utils/Utils';
 import AuthApiService from '../../services/auth-api-service';
+import ValidationError from '../../ValidationError';
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
 export default class RegistrationForm extends Component {
 	static defaultProps = {
 		onRegistrationSuccess: () => {}
 	};
+	// error is the result from the POST to the API
+	// errors holds the form field change before the POST to the API
+	state = {
+		error: null,
+		formValid: false,
+		errorCount: null,
+		password: '',
+		errors: {
+			password: ''
+		}
+	};
 
-	state = { error: null };
+	updateErrorCount = () => {
+		let errors = this.state.errors;
+		let count = 0;
 
-	handleSubmit = ev => {
-		ev.preventDefault();
-		const { username, password, fullname } = ev.target;
+		Object.values(errors).forEach(val => {
+			if (val.length > 0) {
+				count++;
+			}
+		});
+
+		this.setState({ errorCount: count });
+		let valid = count === 0 ? true : false;
+		this.setState({ formValid: valid });
+	};
+
+	validatePassword(password) {
+		if (password.length < 8) {
+			return 'Password must be longer than 7 characters';
+		}
+		if (password.length > 72) {
+			return 'Password must be less than 72 characters';
+		}
+		if (password.startsWith(' ') || password.endsWith(' ')) {
+			return 'Password must not start or end with empty spaces';
+		}
+		if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+			return 'Password must contain one upper case, lower case, number and special character';
+		}
+		return '';
+	}
+
+	validateField = (name, value) => {
+		let err = '';
+
+		if (name === 'password') {
+			err = this.validatePassword(value);
+		}
+
+		const { errors } = { ...this.state };
+		errors[name] = err;
+		this.setState({ errors });
+	};
+
+	handleChange = event => {
+		const { name, value } = event.target;
+		this.setState({ [name]: value.trim() });
+
+		this.validateField(name, value);
+		this.updateErrorCount();
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		const { username, password, fullname } = event.target;
 
 		this.setState({ error: null });
 		AuthApiService.postUser({
@@ -31,7 +94,7 @@ export default class RegistrationForm extends Component {
 	};
 
 	render() {
-		const { error } = this.state;
+		const { error, errors } = this.state;
 		return (
 			<form className="RegistrationForm" onSubmit={this.handleSubmit}>
 				<div role="alert">{error && <p className="error">{error}</p>}</div>
@@ -55,8 +118,16 @@ export default class RegistrationForm extends Component {
 						name="password"
 						type="password"
 						required
-						id="RegistrationForm__password"
+						id="password"
+						aria-required="true"
+						aria-describedby="passwordError"
+						aria-invalid="true"
+						onChange={this.handleChange}
 					/>
+					{errors.password.length > 0 && (
+						<ValidationError message={errors.password} id={'passwordError'} />
+					)}
+					<br />
 				</div>
 				<button className="btn-basic">Register</button>
 			</form>
