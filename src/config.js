1711238fb4977f1537033e6eae866f0b555ac7a4
Tami Williams <tami@asktami.com>
2019-12-02 17:04:04 -0500

    removed extra slash at end of url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // hosted on Heroku using PostgreSQL db
 export default {
 	DATASOURCE: `postgresql`,
-	API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api/`,
-	AUTH_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api/`,
+	API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
+	AUTH_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
 	TOKEN_KEY: process.env.REACT_APP_API_KEY
 };
 
