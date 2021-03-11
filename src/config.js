03ba1a1ce3f24653c3654aba147068805a00f421
Tami Williams <tami@asktami.com>
2021-03-11 16:56:17 -0500

    changed cors anywhere url

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 // hosted on Heroku using PostgreSQL db
-// export default {
-// 	DATASOURCE: `postgresql`,
-// 	API_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
-// 	AUTH_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
-// 	TOKEN_KEY: process.env.REACT_APP_API_KEY,
-// };
-
-// hosted locally using PostgreSQL db
 export default {
 	DATASOURCE: `postgresql`,
-	API_ENDPOINT: `http://localhost:8000/api`,
-	AUTH_ENDPOINT: `http://localhost:8000/api`,
+	API_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
+	AUTH_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-sessionhero-api.herokuapp.com/api`,
 	TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
+
+// hosted locally using PostgreSQL db
+// export default {
+// 	DATASOURCE: `postgresql`,
+// 	API_ENDPOINT: `http://localhost:8000/api`,
+// 	AUTH_ENDPOINT: `http://localhost:8000/api`,
+// 	TOKEN_KEY: process.env.REACT_APP_API_KEY,
+// };
